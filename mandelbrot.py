b0fed6d2e6f668ef68200231aad45bd18eca4f92
caleb-james-smith <caleb_smith2@baylor.edu>
2020-09-07 10:26:21 -0600

    plot standard mandelbrot

--- mandelbrot.py
+++ mandelbrot.py
@@ -60,23 +60,23 @@ def drawPureLight(x, y, c, N, draw):
     draw.point([x, y], (color, color, color))
 
 def drawIterDark(x, y, c, N, draw):
-    #m = mandelbrot(c, N)
+    m = mandelbrot(c, N)
     #m = mandelbrotCube(c, N)
-    m = mandelbrotFourth(c, N)
+    #m = mandelbrotFourth(c, N)
     color = 0 + int(255 * m / N)
     draw.point([x, y], (color, color, color))
 
 def drawIterLight(x, y, c, N, draw):
-    #m = mandelbrot(c, N)
+    m = mandelbrot(c, N)
     #m = mandelbrotCube(c, N)
-    m = mandelbrotFourth(c, N)
+    #m = mandelbrotFourth(c, N)
     color = 255 - int(255 * m / N)
     draw.point([x, y], (color, color, color))
 
 def drawColor(x, y, c, N, draw):
-    #m = mandelbrot(c, N)
+    m = mandelbrot(c, N)
     #m = mandelbrotCube(c, N)
-    m = mandelbrotFourth(c, N)
+    #m = mandelbrotFourth(c, N)
     hue = int(255 * m / N)
     saturation = 255
     value = 255 if m < N else 0
@@ -85,43 +85,43 @@ def drawColor(x, y, c, N, draw):
 def plot():
     COLOR_MODE = False
     PURE_MODE  = False
-    DARK_MODE  = False
-    MAX_ITER = 100
+    DARK_MODE  = True
+    MAX_ITER = 20
 
     # note: width to height ratio should match real to imaginary range ratio to maintain shape similarity
   
     # --- standard dimensions
     # Image size (pixels)
-    #WIDTH  = 1200
-    #HEIGHT = 800
-    
-    # Plot window
-    #RE_START    = -2
-    #RE_END      =  1
-    #IM_START    = -1
-    #IM_END      =  1
-    
-    # --- cube dimensions
-    ## Image size (pixels)
-    #WIDTH  = 600
-    #HEIGHT = 900
-    #
-    ## Plot window
-    #RE_START    = -1
-    #RE_END      =  1
-    #IM_START    = -1.5
-    #IM_END      =  1.5
-    
-    # --- fourth dimensions
-    # Image size (pixels)
     WIDTH  = 1200
-    HEIGHT = 1200
+    HEIGHT = 800
     
     # Plot window
     RE_START    = -2
-    RE_END      =  2
-    IM_START    = -2
-    IM_END      =  2
+    RE_END      =  1
+    IM_START    = -1
+    IM_END      =  1
+    
+    # --- cube dimensions
+    # # Image size (pixels)
+    # WIDTH  = 600
+    # HEIGHT = 900
+    # 
+    # # Plot window
+    # RE_START    = -1
+    # RE_END      =  1
+    # IM_START    = -1.5
+    # IM_END      =  1.5
+    
+    # --- fourth dimensions
+    # # Image size (pixels)
+    # WIDTH  = 1200
+    # HEIGHT = 1200
+    # 
+    # # Plot window
+    # RE_START    = -2
+    # RE_END      =  2
+    # IM_START    = -2
+    # IM_END      =  2
    
     if COLOR_MODE:
         im = Image.new('HSV', (WIDTH, HEIGHT), (255, 255, 255))
